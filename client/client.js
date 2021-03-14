4cfd4f8509391696940b94462dd3d0d229952334
AnnaKazakova <anna.kazakova@studmail.htw-aalen.de>
2021-03-14 19:01:48 +0100

    delete  plugin-helper

--- client/client.js
+++ client/client.js
@@ -3,12 +3,11 @@ import {
   registerBpmnJSModdleExtension, registerClientExtension
 } from 'camunda-modeler-plugin-helpers';
 
+import ParadigmaPlugin from './global-context/ParadigmaPlugin';
+
 var extendedPropertiesProvider = require('./properties-panel/ExtendedPropertiesProvider');
 var paradigmaModdle = require('./properties-panel/descriptors/paradigma.json');
 
 registerBpmnJSPlugin(extendedPropertiesProvider);
 registerBpmnJSModdleExtension(paradigmaModdle);
-
-import ParadigmaPlugin from './global-context/ParadigmaPlugin';
-
 registerClientExtension(ParadigmaPlugin);
